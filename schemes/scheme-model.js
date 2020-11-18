7e7a4ef2f09827809848ff38124594abd5b7e531
Phil <philip-lisak@lambdastudents.com>
2020-11-17 18:39:27 -0700

    Initial markup for scheme-model.js

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -1,1 +1,57 @@
-// scheme-model
+const db = require('../data/db-config')
+module.exports = {
+    find, 
+    findById,
+    findSteps,
+    add, 
+    update, 
+    remove,
+}
+
+async function find() {
+    try {
+        
+    } catch (err) {
+        
+    }
+}
+
+async function findById(id) {
+    try {
+        
+    } catch (err) {
+        
+    }
+}
+
+async function findSteps(id) {
+    try {
+        
+    } catch (err) {
+        
+    }
+}
+
+async function add(scheme) {
+    try {
+        
+    } catch (err) {
+        
+    }
+}
+
+async function update(changes, id) {
+    try {
+        
+    } catch (err) {
+        
+    }
+}
+
+async function remove(id) {
+    try {
+        
+    } catch (err) {
+        
+    }
+}
