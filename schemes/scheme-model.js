040ef683323a95497547b833a3395db00906443a
Phil <philip-lisak@lambdastudents.com>
2020-11-17 19:02:05 -0700

    Mostly complete, need some help with the steps

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -1,57 +1,66 @@
-const db = require('../data/db-config')
+const db = require('../data/db-config');
+
 module.exports = {
-    find, 
-    findById,
-    findSteps,
-    add, 
-    update, 
-    remove,
-}
+	find,
+	findById,
+	findSteps,
+	add,
+	update,
+	remove,
+};
 
 async function find() {
-    try {
-        
-    } catch (err) {
-        
-    }
+	try {
+		return await db('schemes');
+	} catch (err) {
+		throw err;
+	}
 }
 
 async function findById(id) {
-    try {
-        
-    } catch (err) {
-        
-    }
+	try {
+		const scheme = await db('schemes').where({ id }).first();
+		return scheme;
+	} catch (err) {
+		throw err;
+	}
 }
 
 async function findSteps(id) {
-    try {
-        
-    } catch (err) {
-        
-    }
+	try {
+		const steps = await db('schemes as s')
+			.join('steps as st', 's.id', 'st.scheme_id')
+			.where({ scheme_id: id })
+			.select('st.step_number', 's.scheme_name', 'st.instructions');
+		return steps;
+	} catch (err) {
+		throw err;
+	}
 }
 
 async function add(scheme) {
-    try {
-        
-    } catch (err) {
-        
-    }
+	try {
+		const ids = await db('schemes').insert(scheme);
+		const newScheme = await findById(ids[0]);
+		return newScheme;
+	} catch (err) {
+		throw err;
+	}
 }
 
 async function update(changes, id) {
-    try {
-        
-    } catch (err) {
-        
-    }
+	try {
+		await db('schemes').where({ id }).update(changes);
+		return await findById(id);
+	} catch (err) {
+		throw err;
+	}
 }
 
 async function remove(id) {
-    try {
-        
-    } catch (err) {
-        
-    }
+	try {
+		return await db('schemes').del().where({ id });
+	} catch (err) {
+		throw err;
+	}
 }
